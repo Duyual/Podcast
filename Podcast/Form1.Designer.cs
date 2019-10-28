f443cf20004aac311dd5b991647bc0cfef10a792
Linda Tran <lindatran@live.se>
2019-10-28 14:03:46 +0100

    forms

--- Podcast/Form1.Designer.cs
+++ Podcast/Form1.Designer.cs
@@ -28,13 +28,152 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblUrl = new System.Windows.Forms.Label();
+            this.lblUppdatering = new System.Windows.Forms.Label();
+            this.lblKategori = new System.Windows.Forms.Label();
+            this.textBoxUrl = new System.Windows.Forms.TextBox();
+            this.comboBoxUppdatering = new System.Windows.Forms.ComboBox();
+            this.comboBoxKategori = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ColumnEpisode = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnNamn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnFrekvens = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnKategori = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblUrl
+            // 
+            this.lblUrl.AutoSize = true;
+            this.lblUrl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUrl.Location = new System.Drawing.Point(24, 34);
+            this.lblUrl.Name = "lblUrl";
+            this.lblUrl.Size = new System.Drawing.Size(38, 16);
+            this.lblUrl.TabIndex = 0;
+            this.lblUrl.Text = "URL";
+            // 
+            // lblUppdatering
+            // 
+            this.lblUppdatering.AutoSize = true;
+            this.lblUppdatering.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUppdatering.Location = new System.Drawing.Point(160, 34);
+            this.lblUppdatering.Name = "lblUppdatering";
+            this.lblUppdatering.Size = new System.Drawing.Size(153, 16);
+            this.lblUppdatering.TabIndex = 1;
+            this.lblUppdatering.Text = "Uppdateringsfrekens";
+            // 
+            // lblKategori
+            // 
+            this.lblKategori.AutoSize = true;
+            this.lblKategori.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblKategori.Location = new System.Drawing.Point(330, 34);
+            this.lblKategori.Name = "lblKategori";
+            this.lblKategori.Size = new System.Drawing.Size(66, 16);
+            this.lblKategori.TabIndex = 2;
+            this.lblKategori.Text = "Kategori";
+            // 
+            // textBoxUrl
+            // 
+            this.textBoxUrl.Location = new System.Drawing.Point(27, 65);
+            this.textBoxUrl.Name = "textBoxUrl";
+            this.textBoxUrl.Size = new System.Drawing.Size(114, 20);
+            this.textBoxUrl.TabIndex = 3;
+            // 
+            // comboBoxUppdatering
+            // 
+            this.comboBoxUppdatering.FormattingEnabled = true;
+            this.comboBoxUppdatering.Location = new System.Drawing.Point(163, 65);
+            this.comboBoxUppdatering.Name = "comboBoxUppdatering";
+            this.comboBoxUppdatering.Size = new System.Drawing.Size(138, 21);
+            this.comboBoxUppdatering.TabIndex = 4;
+            // 
+            // comboBoxKategori
+            // 
+            this.comboBoxKategori.FormattingEnabled = true;
+            this.comboBoxKategori.Location = new System.Drawing.Point(333, 64);
+            this.comboBoxKategori.Name = "comboBoxKategori";
+            this.comboBoxKategori.Size = new System.Drawing.Size(107, 21);
+            this.comboBoxKategori.TabIndex = 5;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnEpisode,
+            this.ColumnNamn,
+            this.ColumnFrekvens,
+            this.ColumnKategori});
+            this.dataGridView1.Location = new System.Drawing.Point(27, 122);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(413, 66);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // ColumnEpisode
+            // 
+            this.ColumnEpisode.HeaderText = "Episode";
+            this.ColumnEpisode.Name = "ColumnEpisode";
+            // 
+            // ColumnNamn
+            // 
+            this.ColumnNamn.HeaderText = "Namn";
+            this.ColumnNamn.Name = "ColumnNamn";
+            // 
+            // ColumnFrekvens
+            // 
+            this.ColumnFrekvens.HeaderText = "Frekvens";
+            this.ColumnFrekvens.Name = "ColumnFrekvens";
+            // 
+            // ColumnKategori
+            // 
+            this.ColumnKategori.HeaderText = "Kategori";
+            this.ColumnKategori.Name = "ColumnKategori";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(535, 64);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(227, 124);
+            this.dataGridView2.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBoxKategori);
+            this.Controls.Add(this.comboBoxUppdatering);
+            this.Controls.Add(this.textBoxUrl);
+            this.Controls.Add(this.lblKategori);
+            this.Controls.Add(this.lblUppdatering);
+            this.Controls.Add(this.lblUrl);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblUrl;
+        private System.Windows.Forms.Label lblUppdatering;
+        private System.Windows.Forms.Label lblKategori;
+        private System.Windows.Forms.TextBox textBoxUrl;
+        private System.Windows.Forms.ComboBox comboBoxUppdatering;
+        private System.Windows.Forms.ComboBox comboBoxKategori;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnEpisode;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnNamn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnFrekvens;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnKategori;
+        private System.Windows.Forms.DataGridView dataGridView2;
     }
 }
 
